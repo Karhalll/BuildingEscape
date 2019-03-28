b605c8cbdf92650e57b0ce044eda4407eeb792b7
Martin Fučík <mar.fucik@gmail.com>
2019-03-28 14:45:30 +0100

    Position Report showing actual position in vector coordinates

--- BuildingEspace/Source/BuildingEspace/PositionReport.cpp
+++ BuildingEspace/Source/BuildingEspace/PositionReport.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -18,7 +19,9 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("Position Report reporting for duty on Chair!"));
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetActorLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("Position Report reporting for duty for %s at location %s"), *ObjectName, *ObjectPos);
 	
 }
 
